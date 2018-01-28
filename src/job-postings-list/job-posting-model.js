99e3b8d9b8b5c79fb4db61ad4004c1ac224437ad
Kate Sohng <kheeya7@hotmail.com>
2018-01-28 12:35:58 -0800

    initial routing fix, display imported data

--- src/job-postings-list/job-posting-model.js
+++ src/job-postings-list/job-posting-model.js
@@ -16,4 +16,21 @@ export class JobPostingModel extends Backbone.Model {
         this.set('jobDescription',savedObj.jobDescription);
         this.set('industry', savedObj.industry);
     }
+
+    static getJobPostingModelFromGitHubJob(gitHubJobPosting) {
+        const jobPostingModel = new JobPostingModel();
+
+        this.set('postOrigin', 'github');
+        this.set('id', gitHubJobPosting.id);
+        this.set('createdAt', gitHubJobPosting.created_at);
+        this.set('title', gitHubJobPosting.title);
+        this.set('location', gitHubJobPosting.location);
+        this.set('type', gitHubJobPosting.type);
+        this.set('jobDescription', gitHubJobPosting.description);
+        this.set('howToApply', gitHubJobPosting.how_to_apply);
+        this.set('company', gitHubJobPosting.company);
+        this.set('companyUrl', gitHubJobPosting.company_url);
+        this.set('companyLogo', gitHubJobPosting.company_logo);
+        this.set('remoteUrl', gitHubJobPosting.url);
+    }
 }
