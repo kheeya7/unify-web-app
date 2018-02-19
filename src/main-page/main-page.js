a87d97688a58cedd4bdad6345d7462395b85fa6f
Kate Sohng <kheeya7@hotmail.com>
2018-02-19 12:57:54 -0800

    read and write using firebase db

--- src/main-page/main-page.js
+++ src/main-page/main-page.js
@@ -5,9 +5,25 @@ export class MainPage extends Backbone.View {
     initialize() {
         this.currentUser = window.unifyApp.currentUser;
     }
+
+    get events() {
+        return {
+            'click .go-to-import': 'onGoToImport',
+            'click .go-to-job-postings': 'onGoToJobPostings',
+        };
+    }
+
     render() {
         this.$el.html(template({
             currentUser: this.currentUser,
         }));
+    }
+
+    onGoToImport() {
+        window.unifyApp.router.navigate('imports', true);
+    }
+
+    onGoToJobPostings() {
+        window.unifyApp.router.navigate('jobPostings', true);
     }
 }
